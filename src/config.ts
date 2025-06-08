3f085fe82678c4c813e2bed700e0ff133b63650a
masa162 <belong2jazz@gmail.com>
2025-06-08 15:57:44 +0900

    initial commit from isuku template

---
+++ src/config.ts
@@ -0,0 +1,7 @@
+// Place any global data in this file.
+// You can import this data from anywhere in your site by using the `import` keyword.
+
+export const SITE_TITLE = "医スク！ | 医学論文 × 解説ポータル";
+export const SITE_DESCRIPTION = '医スク！ | 医学論文 × 解説ポータル';
+export const GENERATE_SLUG_FROM_TITLE = true
+export const TRANSITION_API = true
