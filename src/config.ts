6323e7fc3f9fe76d497f196e28f66cb6f08a5c78
masa162 <belong2jazz@gmail.com>
2025-06-09 19:05:16 +0900

    rv

--- src/config.ts
+++ src/config.ts
@@ -1,7 +1,7 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = "医スク！ | 医学論文 × 解説ポータル";
-export const SITE_DESCRIPTION = '医スク！ | 医学論文 × 解説ポータル';
+export const SITE_TITLE = "あさおとみどり | 麻生区の自然観察ブログ";
+export const SITE_DESCRIPTION = 'あさおとみどり | 麻生区の自然観察ブログ';
 export const GENERATE_SLUG_FROM_TITLE = true
 export const TRANSITION_API = true
