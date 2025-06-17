d9f75a9f631ba1c9a1d58abf11be2f043c06ff30
masa162 <belong2jazz@gmail.com>
2025-06-17 11:09:29 +0900

    feat: サイト紹介ページ追加 & サムネイル画像ロジック改善

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,10 @@
+// astro.config.mjs
 import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
-import tailwind from "@astrojs/tailwind";
+import tailwind from '@astrojs/tailwind';
 
-// https://astro.build/config
 export default defineConfig({
-  site: 'https://astrofy-template.netlify.app',
-  integrations: [mdx(), sitemap(), tailwind()]
+  site: 'https://asao-green.netlify.app', // ✅ 本番運用ならこちらに
+  integrations: [mdx(), sitemap(), tailwind()],
 });
