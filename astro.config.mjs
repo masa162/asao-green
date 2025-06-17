d26821b01770b827dcb12cf0a5f368497b242fb3
masa162 <belong2jazz@gmail.com>
2025-06-17 11:19:48 +0900

    fix: update astro.config and lockfile

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,6 @@ import sitemap from '@astrojs/sitemap';
 import tailwind from '@astrojs/tailwind';
 
 export default defineConfig({
-  site: 'https://asao-green.netlify.app', // ✅ 本番運用ならこちらに
+  site: 'https://asao-green.netlify.app', // ✅ 本番URLを指定
   integrations: [mdx(), sitemap(), tailwind()],
 });
